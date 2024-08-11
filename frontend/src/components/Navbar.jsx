b260af87cac14be4824c97dede124d2f88b4c185
IndhuC0097 <indhuc0097@gmail.com>
2024-08-11 12:24:36 +0530

    Your commit message

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,27 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import HomeIcon from '@mui/icons-material/Home';
+import AddIcon from '@mui/icons-material/Add';
+import { Box } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: '#9C27B0' }}>
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Employee Management
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          EmployeeApp
         </Typography>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/add">
-          Add Employee
-        </Button>
+        <Box>
+          <IconButton color="inherit">
+            <Home />
+          </IconButton>
+          <IconButton color="inherit">
+            <Add />
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
